5feb2f82c4599a4c159b7f22f14ee2e4ed314761
Moon Chowdhury <68124112+Moon-chowdhury@users.noreply.github.com>
2021-02-06 00:06:33 +0600

    Array split splice

---
+++ array-split.js
@@ -0,0 +1,8 @@
+const numbers= [1,2,3,4,5,6,7,8];
+// const part = numbers.slice(2,5);
+// console.log(part);
+const removed = numbers.splice(2,3, 77, 88,99);
+// console.log(removed);
+// console.log(numbers);
+const togather = numbers.join(" ");
+console.log(togather);
