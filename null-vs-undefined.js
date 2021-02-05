396d62c3423a76473aa0ce279b57a595742ad0bc
Moon Chowdhury <68124112+Moon-chowdhury@users.noreply.github.com>
2021-02-05 14:51:21 +0600

    null Vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+let pakhi;
+ function add(num1, num2){
+   console.log(num1+num2);
+    return
+ }
+
+ function add(num1, num2){
+     console.log(num1, num2);
+
+ const premik ={name:"smart dude", phone:43235};
+  console.log(premik.gfname);
+
+ let fun = undefined;
+ console.log(fun);
+
+const ages =[12,15,22];
+console.log(ages[11]);
